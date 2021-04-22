ecf6679d783282223082066a0938a5ea62cba58d
Ellie Hansen <elliehansen11ads@gmail.com>
2021-04-21 19:48:20 -0600

    completed mini project: cat years

---
+++ catYearsMiniProject/main.js
@@ -0,0 +1,25 @@
+// assign your age to the variable 'myAge'
+let myAge = 26
+// assign the value of '2' to the var 'earlyYears'
+let earlyYears = 2
+// use the multiplication operator to multiply the value saved to earlyYears by 25
+earlyYears *= 25
+// subtract '2' years from 'myAge' and assign to 'laterYears' to account for growth rate of a cat after the first two years
+let laterYears = myAge - 2
+// multiplying laterYears value by '4' totals cat years
+laterYears *= 4
+
+// check your work with console.log()
+
+/* console.log(earlyYears)
+console.log(laterYears) */
+
+// add value of 'earlyYears' and 'laterYears' for the total age
+// assign this value to 'myAgeInCatYears'
+let myAgeInCatYears = earlyYears + laterYears
+
+// assign the value of your name to the variable 'myName'
+let myName = 'Ellie'
+
+// use string concatenation to display how old you are in cat years
+console.log('My name is ' + myName + '. ' + 'I am ' + myAge + ' years old in human years, which is ' + myAgeInCatYears + ' years old in cat years.')
